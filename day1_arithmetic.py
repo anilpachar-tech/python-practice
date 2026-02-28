e47db143126185d75804cda77f2bbabff91660bd
Anil Pachar <anilpachar956@gmail.com>
2026-02-28 19:08:26 +0530

    Add files via upload

---
+++ day1_arithmetic.py
@@ -0,0 +1,20 @@
+#Task
+#The first line contains the sum of the two numbers.
+#The second line contains the difference of the two numbers (first - second).
+#The third line contains the product of the two numbers.
+#Example
+# a = 3
+# b = 5
+#Print the following:
+
+#8
+#-2
+#15
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
